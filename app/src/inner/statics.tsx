d23af83475a1275daf43a00486336af88fd98b2d
Junho Park <dwarfthema@gmail.com>
2023-09-21 11:22:42 +0900

    inner_add birds / book_insert sparkles around the door

--- app/src/inner/statics.tsx
+++ app/src/inner/statics.tsx
@@ -5,7 +5,7 @@ import { Group, MathUtils, Mesh } from "three";
 import { lerp } from "three/src/math/MathUtils.js";
 
 export default function Statics({ movementIntensity = 0.08, ...props }) {
-  const { scene: staticScene } = useGLTF("/models/inner_ground.gltf");
+  const { scene: staticScene } = useGLTF("/models/inner_static.gltf");
   const { nodes: staticNodes } = useGraph(staticScene);
 
   const plantMesh = staticNodes.mesh as Group;
@@ -25,7 +25,7 @@ export default function Statics({ movementIntensity = 0.08, ...props }) {
   return (
     <group>
       {staticsMeshs.map((mesh, index) => {
-        if (mesh.name.includes("cloud")) {
+        if (mesh.name.includes("cloud") || mesh.name.includes("birds")) {
           return (
             <Float
               key={index}
@@ -44,6 +44,76 @@ export default function Statics({ movementIntensity = 0.08, ...props }) {
               />
             </Float>
           );
+        } else if (mesh.name.includes("sign")) {
+          if (mesh.name.includes("base")) {
+            return (
+              <mesh
+                name="sign_base"
+                key={index}
+                geometry={mesh.geometry}
+                material={mesh.material}
+                position={mesh.position}
+                receiveShadow
+                castShadow
+              />
+            );
+          } else if (mesh.name.includes("home")) {
+            return (
+              <mesh
+                name="sign_home"
+                key={index}
+                geometry={mesh.geometry}
+                material={mesh.material}
+                position={mesh.position}
+                receiveShadow
+                castShadow
+                onClick={() => {
+                  var newWindow = window.open("about:blank") as Window;
+                  if (newWindow) {
+                    newWindow.location.href = "https://sillyday.kr/";
+                  }
+                }}
+              />
+            );
+          } else if (mesh.name.includes("sillyday")) {
+            return (
+              <mesh
+                name="sign_sillyday"
+                key={index}
+                geometry={mesh.geometry}
+                material={mesh.material}
+                position={mesh.position}
+                receiveShadow
+                castShadow
+                onClick={() => {
+                  var newWindow = window.open("about:blank") as Window;
+                  if (newWindow) {
+                    newWindow.location.href =
+                      "https://www.instagram.com/sillyday.official/";
+                  }
+                }}
+              />
+            );
+          } else if (mesh.name.includes("sillysally")) {
+            return (
+              <mesh
+                name="sign_sillysally"
+                key={index}
+                geometry={mesh.geometry}
+                material={mesh.material}
+                position={mesh.position}
+                receiveShadow
+                castShadow
+                onClick={() => {
+                  var newWindow = window.open("about:blank") as Window;
+                  if (newWindow) {
+                    newWindow.location.href =
+                      "https://www.instagram.com/sillysally.official/";
+                  }
+                }}
+              />
+            );
+          }
         } else if (mesh.name.includes("Ground") || mesh.name.includes("bush")) {
           return (
             <mesh
@@ -81,14 +151,12 @@ const MovingMesh = ({
   const meshRef = useRef<Mesh | null>(null);
   const [ransomVal, setRansomVal] = useState(Math.random() * 2 * Math.PI);
 
-  console.log(ransomVal);
-
   useFrame(({ clock }) => {
     const mesh = meshRef.current;
     if (mesh) {
       const targetRotation =
         Math.sin(clock.getElapsedTime() + ransomVal) * movementIntensity;
-      mesh.rotation.z = lerp(mesh.rotation.z, targetRotation, 0.05);
+      mesh.rotation.y = lerp(mesh.rotation.y, targetRotation, 0.05);
     }
   });
 
--- app/src/scenePortal.tsx
+++ app/src/scenePortal.tsx
@@ -1,16 +1,10 @@
-import { useControls } from "leva";
-import { useEffect, useState } from "react";
-import Sign from "./inner/sign";
-import { Gltf } from "@react-three/drei";
 import Elephant from "./inner/elephant";
 import Statics from "./inner/statics";
-import AnimModel from "./animModel";
 
 export function ScenePortal() {
   return (
     <group position={[0, 0, 0]} name="innerObject">
-      <Sign />
-      <Statics movementIntensity={0.08} />
+      <Statics movementIntensity={0.4} />
       <Elephant />
     </group>
   );
